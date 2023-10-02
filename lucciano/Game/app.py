33c95c9c83f031e27df5079fc984ffed6582babe
Luciano <134172776+RukaRey@users.noreply.github.com>
2023-10-02 17:08:48 -0300

    Add files via upload

--- lucciano/Game/app.py
+++ lucciano/Game/app.py
@@ -1,49 +1,99 @@
-# finais:
-
-'''
-Interface gráfica TKInter
-Armazenamento de dados em arquivo
-Leitura de dados em arquivo
-Dados estruturados na forma de listas E dicionários
-Busca por informações no arquivo em disco ou em dados em tempo de execução
-Classificação de dados usando dois ou mais critérios
-As funcionalidades do programa devem ser de fato escritas na forma de funções (demonstrem a capacidade de criar funções com e sem Parâmetros).
-Usem comentários no código para facilitar a compreensão do que foi criado por parte do avaliador.
-'''
-
-# bom: (1,3,4)
-# neu: (2,3,4)
-# rui: (1,3,2)
-
-floresta=False
-estrada=False
-pantano=False
-quatro=False
-
-for i in range(0,3):
-    esc=int(input("Escolhas? (1)(2)(3)(4)"))
-    match esc:
-        case 1:
-            um=True
-            pass
-        case 2:
-            dois=True
-            pass
-        case 3:
-            tres=True
-            pass
-        case 4:
-            quatro=True
-            pass
-        case _:
-            pass
+import pygame
 
 
-if um==True and tres==True and quatro==True:
-    print("Final feliz")
-elif dois==True and tres==True and quatro==True:
-    print("Final Neutro")
-elif um==True and tres==True and dois==True:
-    print("Final Ruim")
-else:
-    pass
+pygame.init()
+
+largura, altura = 800, 600
+tela = pygame.display.set_mode((largura, altura))
+pygame.display.set_caption("Viusal")
+
+#BCG atual
+atual=0
+RealAtual=0
+
+# Coordenadas e tamanho do retângulo
+x, y = 40, 390
+largura_retangulo, altura_retangulo =720, 170
+# Cor do retângulo (no formato RGB)
+cor_retangulo = (200, 200, 200)
+
+
+fonte=pygame.font.Font(None,36)
+texto=f"Teste"
+lista=[]
+bcg=["a.png","b.png"]
+cortexto=(0,0,255)
+textSurface=fonte.render(texto,True,cortexto)
+textRect=textSurface.get_rect()
+
+
+indice_dialogo_atual = 0  # Inicialize com 0 para começar a partir da primeira linha
+linhas_por_clique = 4  # Número de linhas a serem exibidas a cada clique
+
+
+def processarTxt():
+    arquivo = "dialog.txt"
+    with open(arquivo, 'r',encoding='utf-8') as arquivo:
+        for linha in arquivo:
+            lista.append(linha.strip())
+
+def mostrarBCG(atual):
+    background = pygame.image.load(bcg[atual])
+    background = pygame.transform.scale(background, (largura, altura))
+
+    tela.blit(background, (0, 0))
+
+def mostrarText():
+    global textSurface, textRect,x,largura_retangulo,y,altura_retangulo,indice_dialogo_atual,linhas_por_clique,RealAtual
+    atual=RealAtual
+    mostrarBCG(atual)
+    pygame.draw.rect(tela, cor_retangulo, (x, y, largura_retangulo, altura_retangulo))
+
+    yAtual=y+25
+    for i in range(indice_dialogo_atual, indice_dialogo_atual + linhas_por_clique):
+        if i < len(lista):
+            if i==11:
+                RealAtual=1
+                pass
+                # Evento que vai acontecer na linha 11
+                # tela.fill((0,0,0))
+            else:
+                linha = lista[i]
+                textSurface=fonte.render(linha,True,cortexto)
+                tela.blit(textSurface,(x_texto,yAtual))
+                textRect.center=(x+largura_retangulo//2,y+altura_retangulo//2)
+                yAtual+=35
+        else:
+            # Evento que vai acontecer quando as linhas acabarem
+            # tela.fill((0,0,0))
+            pass #Oque vai acontecer depois do dialogo
+            
+    pygame.display.update()
+
+
+
+mostrarBCG(RealAtual)
+processarTxt()
+x_texto = 70
+y_texto = y + (altura_retangulo - textSurface.get_height()) // 2 
+
+executando=True
+while executando:
+    for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                executando = False
+            elif event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                mostrarText()
+                indice_dialogo_atual += linhas_por_clique
+                pass
+
+    
+    
+    
+
+
+    pygame.display.flip()
+
+
+
+pygame.quit()
